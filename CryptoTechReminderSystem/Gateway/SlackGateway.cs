7111b37522735ecd42c80ce8f0cfc06d9adb4fb0
Wen Ting Wang <chubberlisk@outlook.com>
2019-04-01 13:31:55 +0100

    Refactor acceptance test and add console application

--- CryptoTechReminderSystem/Gateway/SlackGateway.cs
+++ CryptoTechReminderSystem/Gateway/SlackGateway.cs
@@ -2,32 +2,55 @@ using System;
 using CryptoTechReminderSystem.DomainObject;
 using CryptoTechReminderSystem.UseCase;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CryptoTechReminderSystem.Gateway
 {
     public class MessageSender : IMessageSender
     {
-        private readonly HttpClient _client;
-        
-        public MessageSender(string httpLocalhost)
+        public class PostMessageRequest
         {
-            _client = new HttpClient {BaseAddress = new Uri(httpLocalhost)};
+            public string channel{ get; set; }
+            public string text { get; set; }
+            public string icon_emoji { get; set; }
+            public bool as_user { get; set; }
+            
+        }
+        
+        private readonly HttpClient _client;
+        private string _token;
+
+        public MessageSender(string address, string token)
+        {
+            _client = new HttpClient {BaseAddress = new Uri(address)};
+            _token = token;
         }
 
         public void Send(Message message)
         {
-            var content = new StringContent(message.UserId);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+
+
+            var request = JsonConvert.SerializeObject(new PostMessageRequest
+            {
+                channel = message.UserId,
+                text = "Please make sure your timesheet is submitted by 13:30 on Friday."
+            });
+            var content = new StringContent(request, Encoding.UTF8,
+                "application/json");
             var result = PostChatMessage(content).Result;
             Console.WriteLine(result);
         }
 
         private async Task<object> PostChatMessage(StringContent content)
         {
-                var requestUrl = "/api/chat.postMessage";
-                var response = await _client.PostAsync(requestUrl, content);
-                var result = await response.Content.ReadAsStringAsync();
-                return result;
+            var requestUrl = "/api/chat.postMessage";
+            var response = await _client.PostAsync(requestUrl, content);
+            var result = await response.Content.ReadAsStringAsync();
+            return result;
         }
     }
 }
