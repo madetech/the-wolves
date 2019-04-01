fc1e2c0eca6277104a25ec7dfb8d4fa1cf872586
Wen Ting Wang <chubberlisk@outlook.com>
2019-04-01 13:31:55 +0100

    Refactor acceptance test and add console application

--- CryptoTechReminderSystem.AcceptanceTest/CryptoTechReminderSystemTests.cs
+++ CryptoTechReminderSystem.AcceptanceTest/CryptoTechReminderSystemTests.cs
@@ -1,51 +1,94 @@
-using System;
 using NUnit.Framework;
 using FluentAssertions;
-using System.Collections.Generic;
 using System.Linq;
 using CryptoTechReminderSystem.Boundary;
 using CryptoTechReminderSystem.Gateway;
 using CryptoTechReminderSystem.UseCase;
 using FluentSim;
 using Newtonsoft.Json;
+using static CryptoTechReminderSystem.Gateway.MessageSender;
+using static Newtonsoft.Json.JsonConvert;
 
 namespace CryptoTechReminderSystem.AcceptanceTest
 {
     public class CryptoTechReminderSystemTests
     {
-        [SetUp]
-        public void Setup()
+        private FluentSimulator _fluentSimulator;
+        private MessageSender _messageSender;
+        private RemindUser _remindUser;
+        
+        public class SlackPostMessageResponse
         {
+            [JsonProperty("ok")] public bool IsOk;
         }
 
-        [Test]
-        public void CanRemindAUser()
+        private static PostMessageRequest GetRequest(ReceivedRequest receivedRequest)
         {
-            var fluentSimulator = new FluentSimulator("http://localhost:8009/");
-            var slackGateway = new MessageSender("http://localhost:8009/");
-            var remindUser = new RemindUser(slackGateway);
-            var remindUserRequest = new RemindUserRequest
-            {
-                UserId = "U172L982"
-            };
+            return DeserializeObject<PostMessageRequest>(receivedRequest.RequestBody);
+        }
+
+        private void GivenSlackRespondsWithOk()
+        {
             var slackPostMessageResponse = new SlackPostMessageResponse
             {
                 IsOk = true
             };
 
-            fluentSimulator.Start();
-            fluentSimulator.Post("/api/chat.postMessage").Responds(slackPostMessageResponse);
-            
-            remindUser.Execute(remindUserRequest);
-
-            fluentSimulator.ReceivedRequests.First().Url.Should().Be("http://localhost:8009/api/chat.postMessage");
+            _fluentSimulator.Post("/api/chat.postMessage").Responds(slackPostMessageResponse);
         }
-        
-    }
 
-    public class SlackPostMessageResponse
-    {
-        [JsonProperty("ok")]
-        public bool IsOk;
+        private void WhenWeRemindUser(string userId)
+        {
+            _remindUser.Execute(new RemindUserRequest
+            {
+                UserId = userId
+            });
+        }
+
+        private void ThenMessageHasBeenPostedToSlack(string userId)
+        {
+            var receivedRequest = _fluentSimulator.ReceivedRequests.First();
+
+            receivedRequest.Url.Should().Be(
+                "http://localhost:8009/api/chat.postMessage"
+            );
+            receivedRequest.Headers["Authorization"].Should().Be(
+                "Bearer xxxx-xxxxxxxxx-xxxx"
+            );
+            GetRequest(receivedRequest).channel.Should().Be(userId);
+            GetRequest(receivedRequest).text.Should().Be(
+                "Please make sure your timesheet is submitted by 13:30 on Friday."
+            );
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _fluentSimulator = new FluentSimulator(
+                "http://localhost:8009/"
+            );
+            _messageSender = new MessageSender(
+                "http://localhost:8009/",
+                "xxxx-xxxxxxxxx-xxxx"
+            );
+            _remindUser = new RemindUser(_messageSender);
+            _fluentSimulator.Start();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _fluentSimulator.Stop();
+        }
+
+        [Test]
+        public void CanRemindAUser()
+        {
+            GivenSlackRespondsWithOk();
+
+            WhenWeRemindUser("U172L982");
+
+            ThenMessageHasBeenPostedToSlack("U172L982");
+        }
     }
 }
---
+++ CryptoTechReminderSystem.Main/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CryptoTechReminderSystem.Boundary;
+using CryptoTechReminderSystem.Gateway;
+using CryptoTechReminderSystem.UseCase;
+
+namespace CryptoTechReminderSystem.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var remindUser = new RemindUser(new MessageSender("https://slack.com/", "xoxb-588882280116-591298073303-717LPMdHNK75oXmywBQQsw8l"));
+            
+            remindUser.Execute(new RemindUserRequest
+            {
+                UserId = "UHC4PEXM2"
+            });
+        }
+    }
+}
