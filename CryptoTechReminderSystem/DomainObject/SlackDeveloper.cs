ecc86dff1db89c95beae2c0287c83e2d39118f78
Rob McHugh <robert@madetech.com>
2019-04-09 14:02:00 +0100

    Removed unused properties from SlackDeveloper domain object

--- CryptoTechReminderSystem/DomainObject/SlackDeveloper.cs
+++ CryptoTechReminderSystem/DomainObject/SlackDeveloper.cs
@@ -3,8 +3,5 @@ namespace CryptoTechReminderSystem.DomainObject
     public class SlackDeveloper : Developer
     {
         public string Id { get; set; }
-        public string Name { get; set; }
-        public string DisplayName { get; set; }
-        public bool IsBot { get; set; }
     }
 }
