241c5127ab03baeb4b25393506f2c84c5ec0b706
Wen Ting Wang <chubberlisk@outlook.com>
2019-04-10 16:02:56 +0100

    Remove email property from SlackDeveloper

--- CryptoTechReminderSystem/DomainObject/SlackDeveloper.cs
+++ CryptoTechReminderSystem/DomainObject/SlackDeveloper.cs
@@ -3,6 +3,5 @@ namespace CryptoTechReminderSystem.DomainObject
     public class SlackDeveloper : Developer
     {
         public string Id { get; set; }
-        public string Email { get; set; }
     }
 }
