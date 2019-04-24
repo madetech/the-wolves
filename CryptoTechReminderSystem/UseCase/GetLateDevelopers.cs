bb11568f5923e5bdbb80fcceb7eb3ce9309b9794
Rob McHugh <robert@madetech.com>
2019-04-24 12:16:45 +0100

    removing whitespace from usecase

--- CryptoTechReminderSystem/UseCase/GetLateDevelopers.cs
+++ CryptoTechReminderSystem/UseCase/GetLateDevelopers.cs
@@ -44,7 +44,6 @@ namespace CryptoTechReminderSystem.UseCase
                     if (slackLateDeveloper != null)
                     {
                         getLateDevelopersResponse.Developers.Add(slackLateDeveloper.Id);
-
                     }
                 }
             }
