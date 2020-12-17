2af99db2dae8729d94e0809a220e9d6cc49e4c0f
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-17 10:43:19 +0000

    Add new properties to TimeSheet class
    Add `is_closed` and `project_manager_id` properties to TimeSheet class.
    
    These properties are necessary to identify timesheets that are not
    closed (i.e. have not been approved) and the
    responsible Project Manager.

--- CryptoTechReminderSystem/DomainObject/TimeSheet.cs
+++ CryptoTechReminderSystem/DomainObject/TimeSheet.cs
@@ -6,10 +6,20 @@ namespace CryptoTechReminderSystem.DomainObject
     {
         [JsonProperty("id")] 
         public int Id { get; set; }
+
         [JsonProperty("spent_date")] 
         public string TimeSheetDate { get; set; }
+
         [JsonProperty("hours")] 
         public double Hours { get; set; }
+
         public int UserId { get; set; }
+        
+        [JsonProperty("is_closed")]
+        public bool IsClosed { get; }
+
+        [JsonProperty("project_manager_id")]
+        public int ProjectManagerId { get; }
+
     }
 }
