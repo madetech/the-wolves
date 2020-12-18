5d146af4d62b13a0ea9ddea3bd6cab9fec47534a
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-18 20:28:34 +0000

    Implement GetProjectManagers... Execute() method
    This commit completes the functionality of
    GetProjectManagersWithOpenTimeEntries.
    
    To note:
    1) To avoid changing the interface, the method returns a
    GetLateBillablePeopleResponse.  We may want to modify
    the interface to a higher level of abstraction later (as the concern is
    Project Managers, not LateBillablePeople), but I didn't want
    to change too much with this PR.
    
    2) Several private methods are straight copies from
    GetLateBillablePeople (e.g. formatting of email addresses for Slack
    and date calculation).  We may want to extract these later.
    
    3) I found it difficult to test this object.  In particular, I struggled
    to get my head around the various types in play.  There are no
    unit tests.  We may decide that the acceptance test is sufficient.  (I'm
    very open to help/feedback/ideas from others on how to test this.)

--- CryptoTechReminderSystem/UseCase/GetProjectManagersWithOpenTimeEntries.cs
+++ CryptoTechReminderSystem/UseCase/GetProjectManagersWithOpenTimeEntries.cs
@@ -1,16 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using CryptoTechReminderSystem.Boundary;
 using CryptoTechReminderSystem.Gateway;
+using CryptoTechReminderSystem.DomainObject;
 
 namespace CryptoTechReminderSystem.UseCase
 {
     public class GetProjectManagersWithOpenTimeEntries : IGetLateBillablePeople
     {
+        private readonly IHarvestBillablePersonRetriever _harvestBillablePersonRetriever;
+        private readonly ITimeSheetRetriever _harvestTimeSheetRetriever;
+        private readonly ISlackBillablePersonRetriever _slackBillablePersonRetriever;
+        private readonly IClock _clock;
+        
         public GetProjectManagersWithOpenTimeEntries(ISlackBillablePersonRetriever slackBillablePersonRetriever, IHarvestBillablePersonRetriever harvestBillablePersonRetriever, ITimeSheetRetriever harvestTimeSheetRetriever, IClock clock) {
-
+            _slackBillablePersonRetriever = slackBillablePersonRetriever;
+            _harvestBillablePersonRetriever = harvestBillablePersonRetriever;
+            _harvestTimeSheetRetriever = harvestTimeSheetRetriever;
+            _clock = clock;
         }
         
-        public GetLateBillablePeopleResponse Execute() {
-            return new GetLateBillablePeopleResponse();
+        public GetLateBillablePeopleResponse Execute() 
+        {
+            var dateFrom = GetStartingDate(_clock.Now());
+            var dateTo = GetEndingDate(_clock.Now());
+            
+            var timeSheets = _harvestTimeSheetRetriever.RetrieveTimeSheets(dateFrom, dateTo);
+            var billablePeople = _harvestBillablePersonRetriever.RetrieveBillablePeople();
+            
+            var harvestGetBillablePeopleResponse = _harvestBillablePersonRetriever.RetrieveBillablePeople();
+            var slackGetBillablePeopleResponse = _slackBillablePersonRetriever.RetrieveBillablePeople();
+
+            var getProjectManagersWithOpenTimeEntriesResponse = new GetLateBillablePeopleResponse
+            {
+                BillablePeople = new List<GetLateBillablePeopleResponse.LateBillablePerson>()
+            };
+
+            foreach (var timeSheet in timeSheets) {
+                if (!timeSheet.IsClosed)
+                {
+                    var projectManager = billablePeople.Where(person => person.Id == timeSheet.ProjectManagerId).First();
+                    var slackProjectManager = slackGetBillablePeopleResponse.SingleOrDefault(billablePerson => String.Equals(RemoveTopLevelDomain(billablePerson.Email), RemoveTopLevelDomain(projectManager.Email), StringComparison.OrdinalIgnoreCase));
+                    
+                    if (slackProjectManager != null)
+                    {
+                        getProjectManagersWithOpenTimeEntriesResponse.BillablePeople.Add(new GetLateBillablePeopleResponse.LateBillablePerson
+                        {
+                            Id = slackProjectManager.Id,
+                            Email = slackProjectManager.Email
+                        });
+                    }
+                }
+            }
+
+            return getProjectManagersWithOpenTimeEntriesResponse;
+        }
+
+        private static DateTimeOffset GetStartingDate(DateTimeOffset currentDateTime)
+        {
+            var daysFromMonday = (7 + (currentDateTime.DayOfWeek - DayOfWeek.Monday)) % 7;
+            
+            return currentDateTime.AddDays(-daysFromMonday);
+        }
+
+        private static DateTimeOffset GetEndingDate(DateTimeOffset currentDateTime)
+        {
+            if (IsEndOfTheMonth(currentDateTime)) return currentDateTime;
+            
+            var daysToFriday = (7 + (DayOfWeek.Friday - currentDateTime.DayOfWeek)) % 7;
+            
+            return currentDateTime.AddDays(daysToFriday);
+        }
+
+        private static bool IsEndOfTheMonth(DateTimeOffset currentDateTime)
+        {
+            return DateTime.DaysInMonth(currentDateTime.Year, currentDateTime.Month) == currentDateTime.Day;
+        }
+        private static string RemoveTopLevelDomain(string email)
+        {
+            return email.Replace(".co.uk", "").Replace(".com","");
         }
     }
 }
