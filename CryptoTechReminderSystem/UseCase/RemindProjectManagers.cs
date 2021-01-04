9a6260b2c938df92ced8ac3bc0a9fbf04794ba8e
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-01-04 16:49:28 +0000

    Create RemindProjectManagers class
    The RemindProjectManagers class sends a Slack DM to each project manager
    of a project with a late TimeEntry awaiting approval.
    
    It's a peer object to RemindLateBillablePeople and
    ListLateBillablePeople.

--- CryptoTechReminderSystem/UseCase/RemindProjectManagers.cs
+++ CryptoTechReminderSystem/UseCase/RemindProjectManagers.cs
@@ -5,12 +5,27 @@ namespace CryptoTechReminderSystem.UseCase
 {
     public class RemindProjectManagers
     {
+        private readonly IGetLateBillablePeople _getProjectManagersWithOpenTimeEntries;
+        private readonly ISendReminder _sendReminder;
         public RemindProjectManagers(IGetLateBillablePeople getProjectManagersWithOpenTimeEntries, ISendReminder sendReminder)
         {
+            _getProjectManagersWithOpenTimeEntries = getProjectManagersWithOpenTimeEntries;
+            _sendReminder = sendReminder;
         }
         
-        public void Execute(RemindLateBillablePeopleRequest remindLateBillablePeopleRequest)
+        public void Execute(RemindLateBillablePeopleRequest remindProjectManagersRequest)
         {
+            var projectManagersWithOpenTimeEntries = _getProjectManagersWithOpenTimeEntries.Execute();
+
+            foreach (var projectManager in projectManagersWithOpenTimeEntries.BillablePeople)
+            {
+                _sendReminder.Execute(new SendReminderRequest
+                {
+                    Channel = projectManager.Id,
+                    Text = remindProjectManagersRequest.Message,
+                    Email = projectManager.Email
+                });
+            }
         }
     }
 }
