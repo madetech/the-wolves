5dec802cf5c2e12004f2cd1631bd68e35ae0fb0b
MadeTech Dushan <dushan@madetech.com>
2021-07-14 15:23:43 +0100

    Delete ListLateBillablePeopleRequest file

--- CryptoTechReminderSystem/Boundary/ListLateBillablePeopleRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace CryptoTechReminderSystem.Boundary
-{
-    public class ListLateBillablePeopleRequest
-    {
-        public string LateBillablePeopleMessage { get; set; }
-        public string NoLateBillablePeopleMessage { get; set; }
-        public string Channel { get; set; }
-    }
-}
